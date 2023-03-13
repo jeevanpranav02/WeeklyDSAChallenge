062ce17d61e2261133d59ab96f735170512b8cd2
Pranav Jothimani <pranav.2001146@srec.ac.in>
2023-03-13 21:45:20 +0530

    Added Pair Class

---
+++ Week 13/TreeNode/Pair.java
@@ -0,0 +1,20 @@
+package Node;
+
+
+public class Pair {
+    TreeNode key;
+    int value;
+
+    public Pair(TreeNode key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public TreeNode getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
